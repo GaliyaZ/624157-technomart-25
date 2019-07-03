1456c4acf847b3460be3c4f001b49ae859dbe849
GaliyaZ <47469771+GaliyaZ@users.noreply.github.com>
2019-07-03 10:17:09 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,11 +7,16 @@ var linkMap = document.querySelector(".map-image");
 var popupMap = document.querySelector(".map");
 var closeMap = popupMap.querySelector(".close-button");
         
-var addBookmark = document.querySelector(".bookmark-button");
+var addBookmark = document.querySelectorAll(".bookmark-button");
 var bookmark = document.querySelector(".bookmarks");
 
 var addCard = document.querySelector(".buy-button");
 var card = document.querySelector(".card");
+var i = 0;
+
+var linkBuy = document.querySelectorAll(".buy-button");
+var popupBuy = document.querySelector(".add-form");
+var closeBuy = popupBuy.querySelector(".close-button");
 
 linkWrite.addEventListener("click", function(evt) {
   evt.preventDefault();
@@ -35,29 +40,23 @@ closeMap.addEventListener("click", function(evt) {
   popupMap.classList.remove("show-popup");
 }); 
 
-var linkBuy = document.querySelector(".buy-button");
-/*var popupBuy = document.querySelector(".add-form");
-var closeBuy = popupBuy.querySelector(".close-button");*/
-
-linkBuy.addEventListener("click", function(evt) {
-    /*evt.preventDefault();
-    popupBuy.classList.add("show-popup");*/
-    console.log("hhhhhhhhhhhhhhhhhhhhhh");
-});
-/*
-closeBuy.addEventListener("click", function(evt) {
+/* popup добавления в корзину */
+for(i = 0; i < linkBuy.length; i++){ 
+  linkBuy[i].addEventListener("click", function(evt) {
     evt.preventDefault();
-    popupBuy.classList.remove("show-popup");
-}); */
-
-/*фон закладок и корзины*/
-addBookmark.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    bookmark.classList.add("function-background");
-}); 
-
-
-addCard.addEventListener("click", function(evt) {
-    evt.preventDefault();
+    popupBuy.classList.add("show-popup");
     card.classList.add("function-background");
-});
+  });
+};
+closeBuy.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popupBuy.classList.remove("show-popup");
+}); 
+i=0;
+/*фон закладок и корзины*/
+for(i = 0; i < addBookmark.length; i++){ // проходим циклом по всем элементам объекта
+  addBookmark[i].addEventListener("click", function(evt) {
+  evt.preventDefault();
+  bookmark.classList.add("function-background");
+}); 
+};
